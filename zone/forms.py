73d44edf45846f27be07ff99cbf3eb2a15c9bbef
zizoubrown <zizumoha2015@gmail.com>
2020-11-03 07:30:43 +0300

    add neighborhood form

--- zone/forms.py
+++ zone/forms.py
@@ -17,3 +17,9 @@ class UpdateProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ('user', 'neighbourhood')
+
+
+class NeighbourHoodForm(forms.ModelForm):
+    class Meta:
+        model = Neighbourhood
+        exclude = ('admin',)
