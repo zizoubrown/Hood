3e4241753c6104de98720d8e6b7ea844c6b28646
zizoubrown <zizumoha2015@gmail.com>
2020-11-03 10:07:12 +0300

    add signup view function

--- zone/views.py
+++ zone/views.py
@@ -7,5 +7,20 @@ from django.contrib.auth.decorators import login_required
 from .forms import SignUpForm, BusinessForm,UpdateProfileForm, NeighbourHoodForm, PostForm
 
 # Create your views here.
+@login_required(login_url='login')
 def index(request):
     return render(request, 'index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
