000d7720520811a7790d6c135190916f303556bc
zizoubrown <zizumoha2015@gmail.com>
2020-11-03 07:29:51 +0300

    add profile update form

--- zone/forms.py
+++ zone/forms.py
@@ -11,3 +11,9 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ('user', 'neighbourhood')
