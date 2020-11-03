81f24ba1ba9c8d73444708db8f3f15a6da35bcac
zizoubrown <zizumoha2015@gmail.com>
2020-11-03 07:20:46 +0300

    add index view function

--- zone/views.py
+++ zone/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Neighbourhood, Profile, Business, Post 
+from django.contrib.auth.models import User
+
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
