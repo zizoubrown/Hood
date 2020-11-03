4698a7257221f78cb46c47832ae3dd69a78aba32
zizoubrown <zizumoha2015@gmail.com>
2020-11-03 07:25:12 +0300

    add imports

--- zone/views.py
+++ zone/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Neighbourhood, Profile, Business, Post 
 from django.contrib.auth.models import User
-
+from django.http import HttpResponseRedirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
