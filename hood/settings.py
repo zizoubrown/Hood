85b6116fd5b51348802592d076833ef7175ca9ef
zizoubrown <zizumoha2015@gmail.com>
2020-11-03 06:57:35 +0300

    add postgresql to settings

--- hood/settings.py
+++ hood/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'hood.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'hood',
+        'USER':  'postgres',
+        'PASSWORD': 'postgres',
     }
 }
 
