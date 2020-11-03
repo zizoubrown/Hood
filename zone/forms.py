3266d3466a0793edc1b4c87262d3ec156d8f9f4e
zizoubrown <zizumoha2015@gmail.com>
2020-11-03 07:28:11 +0300

    add signup form

---
+++ zone/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Neighbourhood, Profile, Post, Business
+
+
+class SignUpForm(UserCreationForm):
+    username = forms.CharField(max_length=50)
+    email = forms.CharField(max_length=100)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
