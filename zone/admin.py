cc1bf7ffeb49fc41d916ac7a9b7ba170807ec292
zizoubrown <zizumoha2015@gmail.com>
2020-11-03 07:04:45 +0300

    register models

--- zone/admin.py
+++ zone/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Profile, Neighbourhood, Post, Business
+
+admin.site.site_header = "Hood Admin"
+admin.site.site_title = "Hood Admin Area"
+admin.site.index_title = "Welcome to Hood admin"
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Neighbourhood)
+admin.site.register(Post)
+admin.site.register(Business)
