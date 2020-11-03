d7f04a20295d092b7c8bd2e616932f44a6926d10
zizoubrown <zizumoha2015@gmail.com>
2020-11-03 07:35:06 +0300

    add form import

--- zone/views.py
+++ zone/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
+from .forms import SignUpForm, BusinessForm,UpdateProfileForm, NeighbourHoodForm, PostForm
 
 # Create your views here.
 def index(request):
