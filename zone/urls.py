7f8b23ce7a31cc09fd2a49bed9ff65069d8d8dc1
zizoubrown <zizumoha2015@gmail.com>
2020-11-04 09:29:47 +0300

    debug

--- zone/urls.py
+++ zone/urls.py
@@ -7,11 +7,11 @@ urlpatterns = [
     path('account/', include('django.contrib.auth.urls')),
     path('profile/<username>/edit/', views.edit_profile, name='edit-profile'),
     path('profile/<username>', views.profile, name='profile'),
-    path('all-hoods/',views.neighbourhoods,name='hood'),
-    path('new-hood/', views.create_neighbourhood, name='new-hood'),
-    path('join_hood/<id>', views.join_neighbourhood, name='join-hood'),
-    path('leave_hood/<id>', views.leave_neighbourhood, name='leave-hood'),
-    path('single_hood/<hood_id>', views.single_neighbourhood, name='single-hood'),
+    path('all-hoods/',views.neighborhoods,name='hood'),
+    path('new-hood/', views.create_neighborhood, name='new-hood'),
+    path('join_hood/<id>', views.join_neighborhood, name='join-hood'),
+    path('leave_hood/<id>', views.leave_neighborhood, name='leave-hood'),
+    path('single_hood/<hood_id>', views.single_neighborhood, name='single-hood'),
     path('<hood_id>/new-post', views.create_post, name='post'),
     
 ]
